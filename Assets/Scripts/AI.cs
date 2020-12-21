58ee7baec8408445b04a70df13f2f2135ed31bc0
Inessa Pokromkina <atnovemb@gmail.com>
2020-12-21 11:33:23 +0300

    Add Goal Check!

---
+++ Assets/Scripts/AI.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AI : MonoBehaviour{
+    public float rangerDenfece, speed;
+    public Transform denfece;
+    private GameObject _ball;
+    private Rigidbody2D _rb_AI;
+    public bool canShootAI, canHeadAI, grounded;
+    public Transform checkGround;
+    public LayerMask ground_layer;
+    
+    // Start is called before the first frame update
+    void Start(){
+        _ball = GameObject.FindGameObjectWithTag("Ball");
+        // why can't we take AI with tag? what difference between GetComponent and GameObject.GetWithTag ?
+        _rb_AI = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update(){
+        Move();
+        if (canShootAI == true){
+            Shoot();
+        }
+
+        if (canHeadAI == true){
+            Jump();
+        }
+    }
+    
+    private void FixedUpdate() {
+        grounded = Physics2D.OverlapCircle(checkGround.position, 0.2f, ground_layer);
+    }
+
+    public void Move(){
+        if (Mathf.Abs(_ball.transform.position.x - transform.position.x) < rangerDenfece){
+            if (_ball.transform.position.x > transform.position.x){
+                _rb_AI.velocity = new Vector2(Time.deltaTime * speed, _rb_AI.velocity.y);
+            }
+            else{
+                _rb_AI.velocity = new Vector2(-Time.deltaTime * speed, _rb_AI.velocity.y);
+            }
+        }
+        else{
+            if (transform.position.x > denfece.position.x){
+                _rb_AI.velocity = new Vector2(-Time.deltaTime * speed, _rb_AI.velocity.y);
+            }
+            else{
+                _rb_AI.velocity = new Vector2(0, _rb_AI.velocity.y);
+            }
+        }
+    }
+
+    public void Shoot(){
+        if (canShootAI == true) {
+            _ball.GetComponent<Rigidbody2D>().AddForce(new Vector2(200, 300));
+        }
+    }
+
+    public void Jump(){
+        if (grounded == true){
+            _rb_AI.velocity = new Vector2(-Time.deltaTime * speed, 15);
+        }
+    }
+}
--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Ball : MonoBehaviour
 {
     private GameObject _player, _AI;
+	public GameObject goals; 
 
     // Start is called before the first frame update
     void Start()
@@ -29,6 +30,14 @@ public class Ball : MonoBehaviour
 
         if (collision.gameObject.tag == "canHeadAI"){
             _AI.GetComponent<AI>().canHeadAI = true;
+        }
+
+		if (collision.gameObject.tag == "GoalsRight"){
+            Instantiate(goals, new Vector3(0, -2, 0), Quaternion.identity); 
+        }
+
+		if (collision.gameObject.tag == "GoalsLeft"){
+            Instantiate(goals, new Vector3(0, -2, 0), Quaternion.identity); 
         }
     }
 
